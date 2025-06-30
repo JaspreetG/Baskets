0701ff36f3ae4cff7ef00b7cb88493c6224fcd67
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2025-07-01 00:55:42 +0530

    fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.30df7qv6jtg"
+    "revision": "0.vrnd060fgt8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, y: 16 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 16 }}
+        transition={{ duration: 0.35, ease: "easeInOut" }}
+      >
+        <Outlet />
+      </motion.div>
+    </AnimatePresence>
+  );
 }
