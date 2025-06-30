c62d94578db4feeb79b05912b423fe9219aa3634
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2025-07-01 00:49:49 +0530

    fixed strict mode

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oaij5fl5qeg"
+    "revision": "0.30df7qv6jtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
 import { registerSW } from "virtual:pwa-register";
 
 import App from "./App";
